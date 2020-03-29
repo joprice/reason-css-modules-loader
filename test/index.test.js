9dfd5edf8848d3fcde95f88d22518c53bf17e227
Joseph Price <pricejosephd@gmail.com>
2020-03-29 09:38:18 -0400

    update libs

--- test/index.test.js
+++ test/index.test.js
@@ -1,91 +1,87 @@
 const {
-    pathAndFilename,
-    filterNonCamelCaseNames,
-    filterKeywords,
-    makeCssModuleType,
-    finalDestDir,
-} = require('../src/index')
+  pathAndFilename,
+  filterNonCamelCaseNames,
+  filterKeywords,
+  makeCssModuleType,
+  finalDestDir
+} = require("../src/index");
+const path = require("path");
 
-test('correct path and filename', () => {
-    const { currentDir, destFilename } = pathAndFilename("C:\\path\\to\\the\\File.css");
+test("correct path and filename", () => {
+  const { currentDir, destFilename } = pathAndFilename(
+    "C:\\path\\to\\the\\File.css",
+    path.win32
+  );
 
-    expect(currentDir).toBe("C:\\path\\to\\the");
-    expect(destFilename).toBe("FileStyles.re");
-})
+  console.log("dir!", currentDir, destFilename);
+  expect(currentDir).toBe("C:\\path\\to\\the");
+  expect(destFilename).toBe("FileStyles.re");
+});
 
-test('filter non-camel-cased items', () => {
-    let classNames = [
-        'red',
-        'blue',
-        'is-read',
-        'is_read',
-        'isRead',
-        'title-box',
-        'titleBox',
-    ]
+test("filter non-camel-cased items", () => {
+  let classNames = [
+    "red",
+    "blue",
+    "is-read",
+    "is_read",
+    "isRead",
+    "title-box",
+    "titleBox"
+  ];
 
-    expect(filterNonCamelCaseNames(classNames)).toEqual([
-        'red',
-        'blue',
-        'isRead',
-        'titleBox',
-    ])
-})
+  expect(filterNonCamelCaseNames(classNames)).toEqual([
+    "red",
+    "blue",
+    "isRead",
+    "titleBox"
+  ]);
+});
 
-test('rename items whose name is reserved word', () => {
-    let classNames = [
-        'red',
-        'and',
-        'forYou',
-        'includeNext',
-        'let',
-    ]
+test("rename items whose name is reserved word", () => {
+  let classNames = ["red", "and", "forYou", "includeNext", "let"];
 
-    let { validNames, keywordNames } = filterKeywords(classNames)
+  let { validNames, keywordNames } = filterKeywords(classNames);
 
-    expect(validNames).toEqual([
-        'red',
-        'forYou',
-        'includeNext',
-    ])
-    expect(keywordNames).toEqual([
-        'and',
-        'let',
-    ])
-})
+  expect(validNames).toEqual(["red", "forYou", "includeNext"]);
+  expect(keywordNames).toEqual(["and", "let"]);
+});
 
-test('create valid ReasonML type', () => {
-    let classNames = [
-        'red',
-        'forYou',
-        'includeNext',
-    ]
+test("create valid ReasonML type", () => {
+  let classNames = ["red", "forYou", "includeNext"];
 
-    expect(makeCssModuleType(classNames)).toBe(`
+  expect(makeCssModuleType(classNames)).toBe(
+    `
 type definition = Js.t({.
     red: string,
     forYou: string,
     includeNext: string,
 })
-    `.trim())
-})
+    `.trim()
+  );
+});
 
-describe('finalDestDir() tests', () => {
-    test('queryDestDir if set in query', () => {
-        let queryDestDir = "C:\\user\\defined\\dir"
-    
-        expect(finalDestDir(queryDestDir, "C:\\current\\directory")).toBe(query.destDir)
-    })
-    
-    test('./src/styles if not set in query', () => {
-        let queryDestDir;
-    
-        expect(finalDestDir(queryDestDir, "C:\\current\\directory")).toBe('./src/styles')
-    })
-    
-    test('currentDirectory if query.destDir is "current"', () => {
-        let queryDestDir = "current";
-    
-        expect(finalDestDir(queryDestDir, "C:\\current\\directory")).toBe("C:\\current\\directory")
-    })
-})
+describe("finalDestDir() tests", () => {
+  test("queryDestDir if set in query", () => {
+    let queryDestDir = "C:\\user\\defined\\dir";
+
+    expect(finalDestDir(queryDestDir, "C:\\current\\directory")).toBe(
+      queryDestDir
+    );
+  });
+
+  test("./src/styles if not set in query", () => {
+    let queryDestDir;
+
+    expect(finalDestDir(queryDestDir, "C:\\current\\directory")).toBe(
+      "./src/styles"
+    );
+  });
+
+  test('currentDirectory if query.destDir is "current"', () => {
+    let queryDestDir = "current";
+
+    expect(finalDestDir(queryDestDir, "C:\\current\\directory")).toBe(
+      "C:\\current\\directory"
+    );
+  });
+});
